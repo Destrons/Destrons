0d0a47251ff8e405a484f4c42780f529c4d9ae2f
Thomas Neves <thomas_neves@ymail.com>
2025-03-14 11:03:33 -0300

    Update contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,9 @@ function Contact() {
     return (
         <section className="contact" id='contacts'>
             <h2>Contato</h2>
-            <p>Email: seuemail@exemplo.com</p>
+            <p>Email: thomas_neves@ymail.com</p>
+            <p>WhatsApp: +55 11 96784-1671</p>
+
         </section>
     );
 }
