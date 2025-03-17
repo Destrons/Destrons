a522d2e6a5d13635555dd16af9e2e9200e16e5df
Thomas Neves <thomas_neves@ymail.com>
2025-03-17 10:50:28 -0300

    delete old archives
    pasta de arquivos antigos deletadas.
    criada a função de navegação.

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,8 @@
-
-const app = document.getElementById('app');
-
-const header = document.createElement('header');
-header.classList.add('header');
-
-const nav = document.createElement('nav');
-nav.classList.add('nav');
-
-const aboutButton = document.createElement('button');
-aboutButton.textContent = 'About Me';
-aboutButton.addEventListener('click', () => scrollToSection('about-me'));
-
-const skillsButton = document.createElement('button');
-skillsButton.textContent = 'Skills';
-skillsButton.addEventListener('click', () => scrollToSection('skills'));
-
-const projectsButton = document.createElement('button');
-projectsButton.textContent = 'Projects';
-projectsButton.addEventListener('click', () => scrollToSection('projects'));
-
-const contactButton = document.createElement('button');
-contactButton.textContent = 'Contact';
-contactButton.addEventListener('click', () => scrollToSection('contacts'));
-
-nav.appendChild(aboutButton);
-nav.appendChild(skillsButton);
-nav.appendChild(projectsButton);
-nav.appendChild(contactButton);
-
-const navDiv = document.createElement('div');
-navDiv.classList.add('navdiv');
-navDiv.style.display = 'flex';
-navDiv.style.justifyContent = 'flex-end';
-navDiv.style.marginRight = '4%';
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  } else {
+      console.error("Section with ID '" + sectionId + "' not found.");
+  }
+}
